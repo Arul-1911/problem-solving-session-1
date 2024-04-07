3866ea1bdb7ae83cab94da3ac507d2163e9f71eb
Arul-1911 <arulpraveen19112002@gmail.com>
2024-04-07 20:54:27 +0530

    update in problem-1

--- problem-1.js
+++ problem-1.js
@@ -3,16 +3,12 @@
 function isPrime(num) {
   if (num <= 1) return false;
   if (num <= 3) return true;
-  let sum = 0;
-
+  
   if (num % 2 === 0 || num % 3 === 0) return false;
 
   for (let i = 5; i * i <= num; i += 6) {
-    sum+= 1; 
     if (num % i === 0 || num % (i + 2) === 0) return false;
   }
-  sum+= 1; 
-  console.log("Number of iterations: ", sum);
   return true;
 }
 
